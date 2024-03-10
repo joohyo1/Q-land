a77267af456473382b5e37dd1976b7465413b7fa
juhyowon <juhyowork@gmail.com>
2024-03-10 21:31:06 +0900

    feat: 귤, 나무 제거

--- q_land/src/index.js
+++ q_land/src/index.js
@@ -1,9 +1,8 @@
 import * as THREE from "three";
 import Stats from "three/examples/jsm/libs/stats.module.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import printTree from "../mesh/tree.js";
-// import printCatCharacter from "../mesh/catCharacter.js";
-import printHallabong from "../mesh/hallabong.js";
+// import printTree from "../mesh/tree.js";
+// import printHallabong from "../mesh/hallabong.js";
 
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 // import { SpotLightHelper } from "three";
@@ -41,25 +40,6 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  //fbx loader
-  // const fbxLoader = new FBXLoader();
-  // fbxLoader.load("../model/dancing.fbx", (object) => {
-  //   mixer = new THREE.AnimationMixer(object);
-  //   const action = mixer.clipAction(object.animations[0]);
-  //   action.play();
-
-  //   object.traverse(function (child) {
-  //     if (child.isMesh) {
-  //       child.castShadow = true;
-  //       child.receiveShadow = true;
-  //     }
-  //   });
-
-  //   scene.add(object);
-
-  //   model.position.set(0, 20, 0);
-  // });
-
   const loader = new GLTFLoader();
 
   //텍스쳐 적용
@@ -68,7 +48,6 @@ function init() {
   // 섬
   // 섬 기본 높이 3.3 정도
   loader.load("../model/island_color.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(0, 0, 0);
@@ -79,7 +58,6 @@ function init() {
 
   //겨울 섬
   // loader.load("../model/island_winter.glb", (glb) => {
-  //   //모델은 이 안에서 수정
   //   const model1 = glb.scene;
   //   scene.add(model1);
   //   console.log(glb);
@@ -92,7 +70,6 @@ function init() {
 
   // //이글루
   // loader.load("../model/igloo.glb", (glb) => {
-  //   //모델은 이 안에서 수정
   //   const model1 = glb.scene;
   //   scene.add(model1);
   //   model1.position.set(25, 1.5, -120);
@@ -103,7 +80,6 @@ function init() {
 
   //여름 집
   // loader.load("../model/house_summer", (glb) => {
-  //   //모델은 이 안에서 수정
   //   const model1 = glb.scene;
   //   scene.add(model1);
   //   model1.position.set(0, 1.5, -1);
@@ -114,7 +90,6 @@ function init() {
 
   // //봄 가을집
   // loader.load("../model/house_spring_autumm", (glb) => {
-  //   //모델은 이 안에서 수정
   //   const model1 = glb.scene;
   //   scene.add(model1);
   //   model1.position.set(0, 1.5, -1);
@@ -129,7 +104,6 @@ function init() {
 
   //우체통
   loader.load("../model/Mailbox.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-50, 0, 30);
@@ -141,7 +115,6 @@ function init() {
 
   //오두막 집
   loader.load("../model/House.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-80, 0, 30);
@@ -153,7 +126,6 @@ function init() {
 
   //버섯
   loader.load("../model/mushroom.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-50, 0, 30);
@@ -165,7 +137,6 @@ function init() {
 
   //기본 나무
   loader.load("../model/basic_tree.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-90, 0, 40);
@@ -177,7 +148,6 @@ function init() {
 
   //작은 나무
   loader.load("../model/basic_small_tree", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-50, 0, 30);
@@ -189,7 +159,6 @@ function init() {
 
   //파란 꽃
   loader.load("../model/Flower_Blue.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-30, 6, 30);
@@ -201,7 +170,6 @@ function init() {
 
   //튤립
   loader.load("../model/Flower_Tulip.glb", (glb) => {
-    //모델은 이 안에서 수정
     const model1 = glb.scene;
     scene.add(model1);
     model1.position.set(-40, 6, 30);
@@ -309,20 +277,6 @@ function init() {
       }
     });
 
-    //   const mixer = new THREE.AnimationMixer(model1);
-    //   const actions = [];
-    //   actions[0] = mixer.clipAction(glb.animations[0]); //디폴트 애니
-    //   actions[1] = mixer.clipAction(glb.animations[1]);
-    //   actions[0].play();
-
-    //   function draw() {
-    //     const delta = clock.getDelta();
-    //     if (mixer) mixer.update(delta); //오류 해결!
-    //     renderer.render(scene, camera);
-    //     renderer.setAnimationLoop(draw);
-    //   }
-    //   draw();
-
     scene.add(model1);
   });
 
@@ -341,24 +295,11 @@ function init() {
   controls.maxDistance = 100;
   // controls.maxPolarAngle = Math.PI / 3;
 
-  //자동 회전
-  // controls.autoRotate = true;
-  // controls.autoRotateSpeed = 10;
-  // //부드럽게 회전시켜줌
-  // controls.enableDamping = true;
-
-  // ambientLight
-  // //첫번째 인자는 빛의 색상, 두번째 인자는 빛의 강도
-  // const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-  // scene.add(ambientLight);
-
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(0, 5, 5);
   directionalLight.target.position.set(0, 2, 2);
   scene.add(directionalLight);
 
-  //그림자 만들기
-  //rendere에 ShadowMap.enabled = true로 설정, 빛.castShadow = true로 설정, 그림자 줄 에셋에 castShadow = true
   renderer.shadowMap.enabled = true;
   directionalLight.castShadow = true;
 
@@ -381,17 +322,6 @@ function init() {
   );
   dlHelper.position.set(0, 6, 0);
   scene.add(dlHelper);
-
-  // //Spolight helper
-  // const slHelper = new THREE.SpotLightHelper(spotLight, 0xff0000);
-  // scene.add(slHelper);
-
-  //Hemisphere Light
-  //하늘을 향하는 면과 바닥을 향하는 면의 색상을 지정해줄 수 있음
-  //그림자 발생 x
-  // const hemisphereLight = new THREE.HemisphereLight(0xffaaaa, 0x00ff00);
-  // scene.add(hemisphereLight);
-  // const geometry = new THREE.DodecahedronGeometry(1);
 
   //axeshelper: 축 생성
   const axesHelper = new THREE.AxesHelper(10);
@@ -433,48 +363,6 @@ function init() {
     specular: 0x2e6ff2,
   });
 
-  // const mesh = new THREE.Mesh(geometry, standard);
-
-  // //1. 위치
-  // mesh.position.x = 2;
-  // mesh.position.y = 1;
-  // //한번에도 변경 가능
-  // mesh.position.set(2, 1, 1);
-
-  //2. 회전. 360도를 해도 제자리가 아니라 각도가 바뀜. 그 이유는 Three.js는 각도 기준이 아닌 라디안 값을 사용하기 때문임. 라디안 값은 360도가 2파이임 ㅠㅠ
-  // mesh.rotation.y = 360;
-  // //아래 메서드 사용하면 라디안 값 일일히 계산 안해줘도 됨
-  // mesh.rotation.y = THREE.MathUtils.degToRad(360);
-
-  // //3. 크기
-  // mesh.scale.x = 1.2;
-  // mesh.scale.z = 0.8;
-
-  // scene.add(mesh);
-
-  // const box = new THREE.Mesh(geometry, basic);
-  // scene.add(box);
-
-  //구
-  // const geo4 = new THREE.SphereGeometry(1);
-  // const obj4 = new THREE.Mesh(geo4, material);
-  // scene.add(obj4);
-
-  //평면
-  // const geo5 = new THREE.PlaneGeometry(1, 1);
-  // const obj5 = new THREE.Mesh(geo5, material);
-  // scene.add(obj5);
-
-  //원
-  // const geo6 = new THREE.CircleGeometry(1, 32);
-  // const obj6 = new THREE.Mesh(geo6, material);
-  // scene.add(obj6);
-
-  //튜브(첫번째 인자 도형의 중심부분에서 튜브의 중심부분까지의 반지름, 두번째 인자: 튜브의 반지름)
-  // const geo7 = new THREE.TorusGeometry(1, 0.3);
-  // const obj7 = new THREE.Mesh(geo7, material);
-  // scene.add(obj7);
-
   window.addEventListener("resize", () => {
     // 1. 카메라의 종횡비
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -493,39 +381,6 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
 
-  const tree1 = printTree();
-  scene.add(tree1);
-  tree1.position.set(-11, 2, -6);
-
-  const tree2 = printTree();
-  scene.add(tree2);
-  tree2.position.set(-40, 6.5, -10);
-
-  const tree3 = printTree();
-  scene.add(tree3);
-  tree3.position.set(0, 2, -8);
-
-  //가장 왼쪽 나무
-  const tree4 = printTree();
-  scene.add(tree4);
-  tree4.position.set(-50, 4, 10);
-
-  //가장 왼쪽 한라봉
-  const hallabong1 = printHallabong();
-  scene.add(hallabong1);
-  hallabong1.position.set(-45, 7.56, -10);
-
-  const hallabong2 = printHallabong();
-  scene.add(hallabong2);
-  hallabong2.position.set(0, 0.8, -9);
-
-  const hallabong3 = printHallabong();
-  scene.add(hallabong3);
-  hallabong3.position.set(10, 0.8, 10);
-
-  // const cat1 = printCatCharacter();
-  // scene.add(cat1);
-  // cat1.position.set(0, 4, 0);
   stats = new Stats();
 }
 
